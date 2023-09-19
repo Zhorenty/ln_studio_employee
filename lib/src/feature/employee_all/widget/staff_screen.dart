8a1f6c037d8f4f6473b530845e1efbe635b18b2c
George <zhorenty@gmail.com>
2023-09-19 16:29:41 +0300

    Pull UP

--- lib/src/feature/employee_all/widget/staff_screen.dart
+++ lib/src/feature/employee_all/widget/staff_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ln_employee/src/common/widget/overlay/modal_popup.dart';
+import 'package:ln_employee/src/feature/employee/model/employee/employee.dart';
+import 'package:ln_employee/src/feature/employee_create/widget/create_employee_screen.dart';
 
 import '/src/common/assets/generated/fonts.gen.dart';
 import '/src/common/utils/extensions/context_extension.dart';
@@ -26,7 +29,10 @@ class StaffScreen extends StatefulWidget {
   State<StaffScreen> createState() => _StaffScreenState();
 }
 
-class _StaffScreenState extends State<StaffScreen> {
+class _StaffScreenState extends State<StaffScreen>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
   late final StaffBloc staffBloc;
 
   @override
@@ -34,6 +40,13 @@ class _StaffScreenState extends State<StaffScreen> {
     super.initState();
     staffBloc = context.read<StaffBloc>();
     _fetchSalonEmployees();
+    initController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   void _fetchSalonEmployees() {
@@ -43,6 +56,12 @@ class _StaffScreenState extends State<StaffScreen> {
         StaffEvent.fetchSalonEmployees(salonBloc.state.currentSalon!.id),
       );
     }
+  }
+
+  void initController() {
+    controller = BottomSheet.createAnimationController(this);
+    controller.duration = const Duration(milliseconds: 700);
+    controller.reverseDuration = const Duration(milliseconds: 350);
   }
 
   @override
@@ -66,76 +85,16 @@ class _StaffScreenState extends State<StaffScreen> {
               if (state.hasStaff) ...[
                 SliverPadding(
                   padding: const EdgeInsets.all(8),
-                  sliver: SliverList.builder(
-                    itemCount: state.staff.length,
-                    itemBuilder: (context, index) {
-                      final employee = state.staff[index];
-                      final user = employee.userModel;
-                      final jobPlace = employee.jobModel;
-
-                      return Container(
-                        margin: const EdgeInsets.symmetric(vertical: 4 + 2),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 4,
-                        ),
-                        decoration: BoxDecoration(
-                          color: context.colorScheme.onBackground,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            AvatarWidget(
-                              radius: 40,
-                              title: '${user.firstName} ${user.lastName}',
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 8 + 2),
-                                Text(
-                                  '${user.firstName} ${user.lastName}',
-                                  style:
-                                      context.textTheme.titleMedium?.copyWith(
-                                    fontFamily: FontFamily.geologica,
-                                  ),
-                                ),
-                                Text(
-                                  jobPlace.name,
-                                  style:
-                                      context.textTheme.labelMedium?.copyWith(
-                                    fontFamily: FontFamily.geologica,
-                                    color: context.colorScheme.primaryContainer,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                StarRating(
-                                  initialRating: employee.stars,
-                                )
-                              ],
-                            ),
-                            const SizedBox.shrink(),
-                            AnimatedButton(
-                              child: const Icon(Icons.edit, size: 20),
-                              onPressed: () {
-                                _refresh();
-                                context.go(
-                                  '/staff/employee',
-                                  extra: employee.id,
-                                );
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
+                  sliver: EmployeeList(staff: state.staff, refresh: _refresh),
                 ),
                 SliverToBoxAdapter(
                   child: GestureDetector(
-                    onTap: () => context.goNamed('/create_employee'),
+                    onTap: () => ModalPopup.show(
+                      context: context,
+                      showDivider: false,
+                      transitionAnimationController: controller,
+                      child: const CreateEmployeeScreen(),
+                    ),
                     child: Container(
                       margin: EdgeInsets.symmetric(
                         horizontal: MediaQuery.sizeOf(context).width / 8,
@@ -156,7 +115,35 @@ class _StaffScreenState extends State<StaffScreen> {
                       ),
                     ),
                   ),
-                )
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  sliver: SliverToBoxAdapter(
+                    child: Column(
+                      children: [
+                        const Divider(),
+                        Text(
+                          'Уволенные сотрудники',
+                          style: context.textTheme.titleMedium?.copyWith(
+                            fontFamily: FontFamily.geologica,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: EdgeInsets.only(
+                    left: 8,
+                    right: 8,
+                    bottom: MediaQuery.sizeOf(context).height / 8,
+                  ),
+                  sliver: EmployeeList(
+                    isDismiss: true,
+                    staff: state.staff,
+                    refresh: _refresh,
+                  ),
+                ),
               ] else
                 SliverFillRemaining(
                   child: Center(
@@ -193,5 +180,89 @@ class _StaffScreenState extends State<StaffScreen> {
     final block = context.read<StaffBloc>().stream.first;
     _fetchSalonEmployees();
     await block;
+  }
+}
+
+// TODO(zhorenty): Refactor
+class EmployeeList extends StatelessWidget {
+  const EmployeeList({
+    super.key,
+    required this.staff,
+    this.refresh,
+    this.isDismiss = false,
+  });
+
+  ///
+  final List<Employee> staff;
+
+  ///
+  final bool isDismiss;
+
+  ///
+  final void Function()? refresh;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemCount: staff.length,
+      itemBuilder: (context, index) {
+        final employee = staff[index];
+        final user = staff[index].userModel;
+        final jobPlace = staff[index].jobModel;
+
+        return employee.isDismiss == isDismiss
+            ? Container(
+                margin: const EdgeInsets.symmetric(vertical: 4 + 2),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 4,
+                ),
+                decoration: BoxDecoration(
+                  color: context.colorScheme.onBackground,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AvatarWidget(
+                      radius: 40,
+                      title: '${user.firstName} ${user.lastName}',
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 8 + 2),
+                        Text(
+                          '${user.firstName} ${user.lastName}',
+                          style: context.textTheme.titleMedium?.copyWith(
+                            fontFamily: FontFamily.geologica,
+                          ),
+                        ),
+                        Text(
+                          jobPlace.name,
+                          style: context.textTheme.labelMedium?.copyWith(
+                            fontFamily: FontFamily.geologica,
+                            color: context.colorScheme.primaryContainer,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        StarRating(initialRating: employee.stars)
+                      ],
+                    ),
+                    const SizedBox.shrink(),
+                    AnimatedButton(
+                      onPressed: () {
+                        refresh;
+                        context.go('/staff/employee', extra: employee.id);
+                      },
+                      child: const Icon(Icons.edit, size: 20),
+                    ),
+                  ],
+                ),
+              )
+            : const SizedBox.shrink();
+      },
+    );
   }
 }
