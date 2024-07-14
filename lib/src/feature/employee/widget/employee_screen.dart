449f5cbca10fe67e0fa7b5b97ebb2364036feaeb
Georgiy Voloshin <zhorenty@gmail.com>
2024-07-14 15:38:08 +0300

    Fix CI

--- lib/src/feature/employee/widget/employee_screen.dart
+++ lib/src/feature/employee/widget/employee_screen.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
